71d28bfd2bb8a0f0ae41941178d94d255bb5bc9f
thenamks1995-ops <thenamks1995@gmail.com>
2026-04-23 22:25:14 +0700

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,20 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'coreluxreview.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
